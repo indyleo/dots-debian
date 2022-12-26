a7671bb125b97881cabe4b0cee3b1730031864f1
Indy <100316787+indyleo@users.noreply.github.com>
2022-12-26 01:06:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -82,5 +82,5 @@ curl -sS https://starship.rs/install.sh | sh
 # Changing Back To The Home Directory
 cd ~
 ### Nala ###
-echo "Run (sudo nala fetch) To Get Faster Speeds?"
+echo "Run (sudo nala fetch) To Get Faster Speeds"
 
