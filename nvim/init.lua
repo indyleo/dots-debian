4193c908995e927e0134315adcad6d262877e2e4
Indy <100316787+indyleo@users.noreply.github.com>
2023-01-03 18:16:32 -0500

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,12 @@
--- line numbers
+- line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- tabs & indentation
 vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
+vim.opt.listchars:append("tab:> ")
+vim.opt.list = true
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 
@@ -17,3 +19,9 @@ vim.opt.cursorline = true
 
 -- appearance
 vim.opt.termguicolors = true
+
+-- wrap text
+vim.opt.wrap = false
+
+-- swap file
+vim.opt.swapfile = false
