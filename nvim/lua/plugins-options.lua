92ab39ab139a4b339b3df784f250032ae9d73f10
Indy <100316787+indyleo@users.noreply.github.com>
2023-02-19 09:28:20 -0500

    Update plugins-options.lua

--- nvim/lua/plugins-options.lua
+++ nvim/lua/plugins-options.lua
@@ -337,15 +337,14 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = { "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "java", "yaml", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+  ensure_installed = { "bash", "json", "lua", "python"}, -- one of "all" or a list of languages
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "python", "css" } },
+	indent = { enable = true, disable = { "python" } },
 })
 
