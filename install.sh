4c5b0d432cedbcf6e06a9d35502384bcd1b4efd0
Indy <100316787+indyleo@users.noreply.github.com>
2023-02-23 21:32:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo dnf upgrade -y
 username=$(id -u -n 1000)
 
 # Installing CLI programs
-sudo dnf install gpg openssl tldr htop trash-cli neofetch neovim vim lolcat cmatrix cmake hidapi-devel g++ cava wget python3 xdotool unzip python3-setuptools ripgrep fd-find luarocks golang-go shellcheck -y
+sudo dnf install gpg openssl tldr htop trash-cli ranger neofetch neovim vim lolcat cmatrix cmake hidapi-devel g++ cava wget python3 xdotool unzip python3-setuptools ripgrep fd-find luarocks golang-go shellcheck -y
 
 # Installing other pkg managers
 sudo dnf install python3-pip npm -y
