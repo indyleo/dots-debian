e50f15fd0848d5debb332229112b8f986a112df2
Indy <100316787+indyleo@users.noreply.github.com>
2023-02-24 23:37:49 -0500

    Update and rename install.sh to install-fedora.sh

--- install-fedora.sh
+++ install-fedora.sh
@@ -2,7 +2,7 @@
 
 # Check if Script is Run as Root
 if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  echo "You must be a root user to run this script, please run sudo ./install-fedora.sh" 2>&1
   exit 1
 fi
 
