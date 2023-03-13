353ebe6617757718199ed21651599fce6a971027
Indy <100316787+indyleo@users.noreply.github.com>
2023-03-13 14:01:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ nala install zsh zsh-syntax-highlighting -y;
 nala install openjdk-8-jdk openjdk-8-jre openjdk-17-jdk openjdk-17-jre -y;
 
 # Installing GUI programs 
-nala install timeshift alacritty mesa-utils transmission-gtk dconf-editor gnome-tweaks solaar virt-manager steam:i386 gnome-extensions-app gnome-shell-extension-manager -y;
+nala install timeshift alacritty mesa-utils transmission-gtk dconf-editor gnome-tweaks solaar virt-manager steam:i386 gnome-extensions-app -y;
 
 # Installing a music player
 nala install rhythmbox rhythmbox-plugins -y;
