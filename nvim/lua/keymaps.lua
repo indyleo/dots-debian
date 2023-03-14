114e65fa13e77fdbf9bd707927c192a685915e92
Indy <100316787+indyleo@users.noreply.github.com>
2023-03-13 20:23:15 -0400

    Update keymaps.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -47,13 +47,16 @@ keymap("n", "<geader>bn", ":BuffergineCycleNext<CR>", opts)
 keymap("n", "<geader>bp", ":BuffergineCyclePrev<CR>", opts)
 
 -- NvimTree
-keymap("n", "<leader>ft", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>f", ":NvimTreeToggle<CR>", opts)
 
 -- Telescope
 keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>fl", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
-keymap("n", "<leader>fs", ":Telescope grep_string<CR>", opts)
+
+-- Git
+keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
 
 -- Comment
 keymap("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
