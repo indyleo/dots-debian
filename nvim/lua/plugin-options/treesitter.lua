ef6487d11d59046eaaf0b9a512a61af1314de509
Indy <100316787+indyleo@users.noreply.github.com>
2023-03-13 21:02:19 -0400

    Update treesitter.lua

--- nvim/lua/plugin-options/treesitter.lua
+++ nvim/lua/plugin-options/treesitter.lua
@@ -13,11 +13,12 @@ end
 configs.setup({
   ensure_installed = { "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "java", "yaml", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
-  highlight = {
+	highlight = {
 		enable = true, -- false will disable the whole extension
+		disable = { "css" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "python" } },
+	indent = { enable = true, disable = { "python", "css" } },
 })
