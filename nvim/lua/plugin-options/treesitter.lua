f27d3bb80a48d594a27b80b28b530dd11a8a9897
Indy <100316787+indyleo@users.noreply.github.com>
2023-03-13 21:01:23 -0400

    Update treesitter.lua

--- nvim/lua/plugin-options/treesitter.lua
+++ nvim/lua/plugin-options/treesitter.lua
@@ -4,8 +4,14 @@ if not status_ok then
 	return
 end
 
+-- Treesitter
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
+
 configs.setup({
-  ensure_installed = { "bash", "json", "lua", "python", "yaml"  }, -- one of "all" or a list of languages
+  ensure_installed = { "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "java", "yaml", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
   highlight = {
 		enable = true, -- false will disable the whole extension
