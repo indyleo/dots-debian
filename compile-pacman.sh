3003a5cc29ddcd4d6f91602d79e9f73ed3689b56
Indy <100316787+indyleo@users.noreply.github.com>
2023-04-05 09:40:31 -0400

    Adding A Arch Linux Install Script (Not Tested)

---
+++ compile-pacman.sh
@@ -0,0 +1,76 @@
+#!/bin/env bash
+
+# Making directories & files 
+mkdir -p ~/Github ~/Img ~/Applications ~/Pictures/Screenshots ~/Scripts ~/.icons ~/.themes ~/.cache/zsh ~/.cache/vim/undodir ~/.local/bin ~/Zsh/Plugins ~/Desktop ~/Documents ~/Downloads ~/Music ~/Pictures ~/Public ~/Videos
+touch ~/.cache/zsh/history 
+
+# Headsetcontrol
+cd ~/Github
+git clone https://github.com/Sapd/HeadsetControl 
+cd HeadsetControl
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+cd ~
+
+# Envycontrol
+cd ~/Github
+git clone https://github.com/bayasdev/envycontrol.git
+cd envycontrol
+sudo pip install .
+cd ~
+
+# Gotop & Lazygit
+go install github.com/cjbassi/gotop@latest github.com/jesseduffield/lazygit@latest
+
+# Pynvim
+pip install pynvim
+
+# Rofi PowerMenu
+cd ~/Github
+git clone https://github.com/jluttine/rofi-power-menu.git
+cd rofi-power-menu
+cp rofi-power-menu ~/.local/bin/
+cd ~
+
+# Rofi Settings
+rofi -modi drun,window,run
+
+# Reload X11 Colors
+cd ~/dotfiles
+mv .Xresources ~/.Xresources
+xrdb -load ~/.Xresources
+cd ~
+
+# Installing yay AUR Helper
+sudo pacman -S --needed git base-devel --noconfirm
+cd ~/Github
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+cd ~
+
+# Random Color Script
+cd ~/Github
+git clone https://gitlab.com/dwt1/shell-color-scripts.git
+cd shell-color-scripts
+sudo make install
+sudo cp completions/_colorscript /usr/share/zsh/site-functions
+cd ~
+
+# Nvidia Setting
+sudo nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
+
+# Moving And Deletig And Adding Files
+cd ~/dotfiles
+mv Wallpapers ~/Pictures/
+mv alacritty awesome conky neofetch nvim picom qutebrowser ranger rofi starship.toml mimeapps.list ~/.config/
+rm ~/.bashrc
+mv .bashrc .zshrc .aliasrc .xsession ~/
+
+# Flatpak Install
+flatpak install com.github.tchx84.Flatseal com.brave.Browser com.bitwarden.desktop xyz.xclicker.xclicker com.github.unrud.VideoDownloader net.brinkervii.grapejuice org.prismlauncher.PrismLauncher
---
+++ install-pacman.sh
@@ -0,0 +1,81 @@
+#!/bin/env bash
+
+# Checking if script is run with root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+# Update packages list and update system
+pacman -Syu
+
+# Installing CLI programs
+pacman -S starship autojump nano yad fzf locate gh tree xsel tty-clock libhidapi-dev gpg openssl tldr htop trash-cli neofetch neovim lolcat cmatrix cmake g++ wget python3 xdotool unzip tar python3-setuptools ripgrep fd-find luarocks ranger golang-go shellcheck python3-venv git build-essential --noconfirm
+
+# Installing other pkg managers
+pacman -S cargo python3-pip npm flatpak --noconfirm
+
+# Installing zsh
+pacman -S zsh zsh-syntax-highlighting zsh-history-substring-search zsh-completions --noconfirm
+
+# Installing java 
+pacman -S openjdk-8-jdk openjdk-8-jre openjdk-17-jdk openjdk-17-jre --noconfirm
+
+# Installing GUI programs 
+pacman -S rpi-imager bleachbit sxiv timeshift mesa-utils transmission-gtk dconf-editor solaar virt-manager steam:i386 --noconfirm
+
+# Installing a music player
+pacman -S rhythmbox rhythmbox-plugins --noconfirm
+
+# Installing media stuff
+pacman -S yt-dlp mpv peek obs-studio kdenlive audacity gimp inkscape vlc --noconfirm
+
+# Installing AwesomeWM Stuff
+pacman -S variety awesome-doc awesome-extra qt5ct qt5-style-plugins xfce4-clipman xterm awesome pcmanfm alacritty rofi feh picom i3lock-fancy lxappearance arandr xclip xbacklight flameshot network-manager-gnome lxpolkit volumeicon-alsa pavucontrol --noconfirm
+
+# Rofi-Calc Script Depeninces
+pacman -S rofi-dev qalc libtool --noconfirm
+
+# Installing Browsers
+pacman -S firefox chromium qutebrowser libjs-pdf --noconfirm
+
+# Script Variables
+username=$(id -u -n 1000)
+
+# Installing fonts 
+pacman -S fonts-font-awesome --noconfirm
+mkdir -p /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip
+unzip FiraCode.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Meslo.zip
+unzip Meslo.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/SourceCodePro.zip
+unzip SourceCodePro.zip -d /home/$username/.fonts
+chown $username:$username /home/$username/.fonts/
+chown $username:$username /home/$username/.fonts/*
+
+# Reloading cache
+fc-cache -vf
+# Removing zip files
+rm ./FiraCode.zip ./Meslo.zip ./SourceCodePro.zip
+
+# Setting up ufw 
+pacman -S ufw --noconfirm
+sleep 2.5
+ufw limit 22/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw default deny incoming
+ufw default allow outgoing
+ufw enable
+
+# Neovim Stuff
+npm i -g neovim tree-sitter-cli
+
+# Pfetch
+wget https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch
+chmod a+x pfetch
+mv pfetch /usr/bin/pfetch
+
+# Starship Prompt
+curl -sS https://starship.rs/install.sh | sh
