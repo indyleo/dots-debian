3f8f1ccd34fbe4d9ba43544f8e117360c6373b39
Indy <100316787+indyleo@users.noreply.github.com>
2023-04-05 20:35:58 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -7,10 +7,10 @@ touch ~/.cache/zsh/history
 # Pynvim
 pip install pynvim
 
-# Installing Cliphist (For Walyand)
+# Installing Cliphist
 go install go.senan.xyz/cliphist@latest
 
-# Installing yay AUR Helper (Needed)
+# Installing yay AUR Helper
 cd ~/Github
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
@@ -30,9 +30,9 @@ yay -S nordic-theme papirus-icon-theme --noconfirm
 # Moving And Deletig And Adding Files
 cd ~/dotfiles
 mv Wallpapers ~/Pictures/
-mv alacritty kitty awesome conky neofetch nvim picom qutebrowser ranger rofi starship.toml mimeapps.list user-dirs.dirs ~/.config/
+mv alacritty kitty neofetch nvim qutebrowser ranger starship.toml mimeapps.list user-dirs.dirs ~/.config/
 rm ~/.bashrc
-mv .bashrc .zshrc .aliasrc .xsession ~/
+mv .bashrc .zshrc .aliasrc ~/
 
 # Nitch
 wget https://raw.githubusercontent.com/unxsh/nitch/main/setup.sh
@@ -41,4 +41,4 @@ chmod a+x setup.sh
 rm setup.sh
 
 # Flatpak Install
-flatpak install com.github.tchx84.Flatseal com.brave.Browser com.bitwarden.desktop com.github.unrud.VideoDownloader net.brinkervii.grapejuice org.prismlauncher.PrismLauncher
+flatpak install com.github.tchx84.Flatseal com.brave.Browser com.bitwarden.desktop net.brinkervii.grapejuice org.prismlauncher.PrismLauncher
