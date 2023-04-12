5dcc95e5fe6cc9337057b41e9303ff37a26ebfed
Indy <100316787+indyleo@users.noreply.github.com>
2023-04-11 21:01:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,7 +61,10 @@ pacman -S xfce4-settings qt5ct qt5-style-plugins lxappearance --noconfirm
 pacman -S gvfs thunar-archive-plugin thunar file-roller --noconfirm
 
 # Installing Awesome WM
-pacman -S awesome xwallpaper picom flameshot lxsession arandr xfce4-clipman xterm rofi i3lock-fancy xclip xbacklight volumeicon-alsa --noconfirm
+pacman -S conky awesome xwallpaper picom flameshot lxsession xfce4-clipman rofi i3lock-fancy volumeicon-alsa --noconfirm
+
+# Installing X11 stuff
+pacman -S arandr xterm xclip xbacklight --noconfirm
 
 # Installing browser
 pacman -S firefox --noconfirm
