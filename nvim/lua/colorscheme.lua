50c87cf5ea03612e3c36ddcd2eeae09bd90a8521
Indy <100316787+indyleo@users.noreply.github.com>
2023-04-13 21:37:50 -0400

    Update colorscheme.lua

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,11 +1,12 @@
 -- Colorscheme
 vim.cmd('colorscheme nord')
+-- require('onenord').setup()
 
 -- Nordic Theme Setup
 vim.g.nord_contrast = true
 vim.g.nord_borders = false
 vim.g.nord_disable_background = true
-vim.g.nord_italic = false
+vim.g.nord_italic = true
 vim.g.nord_uniform_diff_background = true
 vim.g.nord_bold = false
 
