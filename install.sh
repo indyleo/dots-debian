4ad646d726e801dd4388ddf59c61bbb930f596a7
Indy <100316787+indyleo@users.noreply.github.com>
2023-04-16 00:02:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,10 +14,10 @@ apt upgrade -y
 apt install nala
 
 # Installing CLI programs
-nala install blueman bluez-tools yad fzf locate gh tree xsel build-essential git cmake libhidapi-dev gpg openssl tldr trash-cli neovim g++ gcc wget curl python3 xdotool unzip tar python3-setuptools autojump luarocks ranger shellcheck python3-venv -y
+nala install blueman bluez-tools yad fzf locate gh tree xsel build-essential git cmake libhidapi-dev gpg openssl tldr trash-cli g++ gcc wget curl python3 xdotool unzip tar python3-setuptools autojump luarocks ranger shellcheck python3-venv -y
 
 # Nvim Stuff
-nala install ripgrep fd-find -y
+nala install ripgrep fd-find neovim -y
 
 # Installing other pkg managers
 nala install python-pip npm flatpak golang-go -y
