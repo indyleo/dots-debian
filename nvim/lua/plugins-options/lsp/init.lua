7eb26858a56922070f6fcc27661284aa75b79ea8
Indy <100316787+indyleo@users.noreply.github.com>
2023-04-16 17:07:52 -0400

    Add files via upload

---
+++ nvim/lua/plugins-options/lsp/init.lua
@@ -0,0 +1,9 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+require "plugins-options.lsp.mason"
+require("plugins-options.lsp.handlers").setup()
+require "plugins-options.lsp.null-ls"
+require "plugins-options.lsp.lsp-zero"
