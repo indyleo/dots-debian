6a826641480fdde848399aa755efd6fa9b59a5dd
Indy <100316787+indyleo@users.noreply.github.com>
2023-04-15 22:20:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-# Adding New Soureces.list
-
 # Updating system
 apt update -y
 apt upgrade -y
@@ -15,17 +13,14 @@ apt upgrade -y
 # Intalling Nala
 apt install nala
 
-# Making Sure Chache Is Gone
-dnf clean all
-
 # Installing CLI programs
-dnf install lsd blueman bluez-utils yad fzf locate gh tree xsel hidapi-devel gpg openssl tldr trash-cli neovim cmake g++ gcc wget curl python3 xdotool unzip tar python3-setuptools autojump luarocks ranger shellcheck python3-pytest-venv git -y
+dnf install lsd blueman bluez-utils yad fzf locate gh tree xsel hidapi gpg openssl tldr trash-cli neovim cmake g++ gcc wget curl python3 xdotool unzip tar python-setuptools autojump luarocks ranger shellcheck python-venv git -y
 
 # Nvim Stuff
-dnf install ripgrep fd-find -y
+nala install ripgrep fd-find -y
 
 # Installing other pkg managers
-dnf install cargo python-pip npm flatpak go -y
+nala install cargo python-pip npm flatpak golang-go -y
 
 # Installing fun stuff
 dnf install neofetch cmatrix tty-clock -y
@@ -37,37 +32,37 @@ dnf install btop htop -y
 dnf install xdg-user-dirs xdg-user-dirs-gtk -y
 
 # Installing shell stuff
-dnf install zsh zsh-syntax-highlighting bash-completion -y
+nala install zsh zsh-syntax-highlighting bash-completion -y
 
 # Installing java 
-dnf install java-11-openjdk java-17-openjdk java-1.8.0-openjdk -y
+nala install java-11-openjdk java-17-openjdk java-1.8.0-openjdk -y
 
 # Installing GUI programs 
-dnf install rpi-imager glava alacritty bleachbit timeshift transmission-gtk dconf-editor solaar virt-manager steam -y
+nala install rpi-imager alacritty bleachbit timeshift transmission-gtk dconf-editor solaar virt-manager steam -y
 
 # Installing a music player
-dnf install rhythmbox -y
+nala install rhythmbox rhythombox-plugins -y
 
 # Installing media stuff
-dnf install yt-dlp mpv peek obs-studio kdenlive audacity gimp inkscape vlc -y
+nala install yt-dlp mpv peek obs-studio kdenlive audacity gimp inkscape vlc -y
 
 # Installing image viewer
-dnf install sxiv -y
+nala install sxiv -y
 
 # Theme stuff
-dnf install xfce4-settings qt5ct lxappearance -y
+nala install qt5ct lxappearance -y
 
 # Installing file stuff 
-dnf install gvfs thunar-archive-plugin thunar file-roller -y
+nala install gvfs thunar-archive-plugin thunar file-roller -y
 
-# Installing Awesome WM
-dnf install conky awesome picom flameshot lxpolkit xfce4-clipman rofi i3lock-fancy volumeicon-alsa -y
+# Installing Awesome
+nala install conky awesome picom flameshot lxpolkit xfce4-clipman rofi i3lock-fancy volumeicon-alsa -y
 
 # Installing X11 stuff
-dnf install arandr xterm xclip xbacklight xwallpaper feh nitrogen -y
+nala install arandr xterm xclip xbacklight xwallpaper feh nitrogen -y
 
-# Installing nvidia stuff
-dnf install nvidia-settings nvidia-dkms nvidia-utils -y
+# Installing glxgears
+nala install mesa-utils
 
 # Script Variables
 username=$(id -u -n 1000)
@@ -80,8 +75,6 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.z
 unzip FiraCode.zip -d /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Meslo.zip
 unzip Meslo.zip -d /home/$username/.fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Ubuntu.zip
-unzip Ubuntu.zip -d /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/SourceCodePro.zip
 unzip SourceCodePro.zip -d /home/$username/.fonts
 chown $username:$username /home/$username/.fonts/
@@ -91,10 +84,10 @@ chown $username:$username /home/$username/.fonts/*
 fc-cache -vf
 
 # Removing zip files
-rm ./FiraCode.zip ./Meslo.zip ./SourceCodePro.zip ./Ubuntu.zip
+rm ./FiraCode.zip ./Meslo.zip ./SourceCodePro.zip 
 
 # Setting up ufw 
-dnf install ufw -y
+nala install ufw -y
 sleep 2.5
 ufw limit 22/tcp
 ufw allow 80/tcp
@@ -107,6 +100,7 @@ systemctl enable ufw
 # Installing SDDM & Theme
 dnf install sddm -y
 systemctl set-default graphical.target
+systemctl diable gdm3.service
 systemctl enable sddm.service
 tar -xzvf sugar-candy.tar.gz -C /usr/share/sddm/themes
 mv $builddir/sddm.conf /etc/sddm.conf
@@ -126,6 +120,3 @@ mv nerdfetch /usr/bin/nerdfetch
 
 # Starship Prompt
 curl -sS https://starship.rs/install.sh | sh
-
-# Screen Tearing Fix
-nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
