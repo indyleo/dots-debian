a6401872d34ca40abcd4c897d05555969d733683
Indy <100316787+indyleo@users.noreply.github.com>
2023-04-18 19:42:55 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -60,7 +60,7 @@ cd ~
 
 # Moving And Deletig And Adding Files
 cd $builddir
-git clone https://github.com/indyleo/Wallpapers.git ~/Pictures/Wallpapers/
+git clone https://github.com/indyleo/Wallpapers ~/Pictures/Wallpapers/
 mv neofetch nvim ranger picom awesome conky alacritty rofi starship.toml mimeapps.list user-dirs.dirs ~/.config/
 rm ~/.bashrc
 mv .bashrc .zshrc .aliasrc .xsession .profile ~/
