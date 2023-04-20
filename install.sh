6d7f76f56650b1103b25c9edede3bed5c098ddeb
Indy <100316787+indyleo@users.noreply.github.com>
2023-04-19 21:29:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -114,12 +114,16 @@ nala install fonts-font-awesome fontconfig -y
 mkdir -p /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip
 unzip FiraCode.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraMono.zip
+unzip FiraMono.zip -d /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Meslo.zip
 unzip Meslo.zip -d /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/SourceCodePro.zip
 unzip SourceCodePro.zip -d /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Ubuntu.zip
 unzip Ubuntu.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/UbuntuMono.zip
+unzip UbuntuMono.zip -d /home/$username/.fonts
 chown $username:$username /home/$username/.fonts/
 chown $username:$username /home/$username/.fonts/*
 
@@ -127,7 +131,7 @@ chown $username:$username /home/$username/.fonts/*
 fc-cache -vf
 
 # Removing zip files
-rm ./FiraCode.zip ./Meslo.zip ./SourceCodePro.zip ./Ubuntu.zip
+rm ./FiraCode.zip /FiraMono.zip ./Meslo.zip ./SourceCodePro.zip ./Ubuntu.zip ./UbuntuMono.zip
 
 # Setting up ufw 
 nala install ufw -y
