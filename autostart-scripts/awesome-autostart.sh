52970730d00d6f161bc328746407a7a1656272fd
Indy <100316787+indyleo@users.noreply.github.com>
2023-04-21 12:42:47 -0400

    Update and rename f to awesome-autostart.sh

---
+++ autostart-scripts/awesome-autostart.sh
@@ -0,0 +1,30 @@
+# Backround Programs
+picom --experimental-backends &
+lxpolkit &
+sxhkd &
+
+# Conky
+killall -q conky &
+sleep 3 && conky -c ~/.config/conky/awesome/conky-01.conkyrc &
+
+# Aplleets
+nm-applet &
+flameshot &
+killall -q volumeicon &
+sleep 3 && volumeicon &
+killall -q solaar &
+sleep 3 && solaar -w hide &
+killall -q xfce4-clipman &
+sleep 3 && xfce4-clipman &
+
+# Wallpapers
+# xwallpaper --stretch ~/Pictures/Wallpapers/nord_mountains.png
+find ~/Pictures/Wallpapers/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
+# feh --randomize --bg-fill ~/Pictures/Wallpapers/ &
+# ~/.fehbg &
+# nitrogen --set-zoom --random ~/Pictures/Wallpapers/ &
+# nitrogen --restore &
+
+# Variety
+killall -q variety &
+sleep 3 && variety &
