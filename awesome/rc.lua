8ca73c3b78e007cdac001a994fdfcc79c930e264
Indy <indy@pop-os.localdomain>
2023-04-20 22:46:52 -0400

    getting rid of useless keybinds

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -256,10 +256,6 @@ root.buttons(gears.table.join(
 globalkeys = gears.table.join(
     awful.key({ modkey,  "Shift"  }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
-    awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
-              {description = "view previous", group = "tag"}),
-    awful.key({ modkey,           }, "Right",  awful.tag.viewnext,
-              {description = "view next", group = "tag"}),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
               {description = "go back", group = "tag"}),
 
