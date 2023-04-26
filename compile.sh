d3e5f3a114a4fcc2fdd84b1faeb8d9040e29446b
Indy <indy@pop-os.localdomain>
2023-04-25 20:38:02 -0400

    removing autostart scripts

--- compile.sh
+++ compile.sh
@@ -61,7 +61,7 @@ cd ~
 # Moving And Deletig And Adding Files
 cd $builddir
 git clone https://github.com/indyleo/Wallpapers.git ~/Pictures/Wallpapers/
-mv  sxhkd autostart-scripts polybar neofetch nvim ranger picom awesome conky alacritty rofi starship.toml mimeapps.list user-dirs.dirs ~/.config/
+mv  sxhkd polybar neofetch nvim ranger picom awesome conky alacritty rofi starship.toml mimeapps.list user-dirs.dirs ~/.config/
 rm ~/.bashrc ~/.profile
 mv .bashrc .zshrc .zshenv .aliasrc .xsession .profile ~/
 
