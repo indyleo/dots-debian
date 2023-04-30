fb79d22771b1455a49a2ab63f8828643fa27b713
Indy <indy@pop-os.localdomain>
2023-04-29 20:49:43 -0400

    making the flathub repo work

--- compile.sh
+++ compile.sh
@@ -125,7 +125,7 @@ chmod a+x setup.sh
 rm setup.sh
 
 # Getting the flathub repo
-flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Flatpak Install
 flatpak install com.github.tchx84.Flatseal com.brave.Browser com.bitwarden.desktop net.brinkervii.grapejuice org.prismlauncher.PrismLauncher xyz.xclicker.xclicker com.github.unrud.VideoDownloader 
