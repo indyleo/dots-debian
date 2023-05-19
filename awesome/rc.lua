f090744fdbafca1a27f46196c029869faa8fa8c9
Indy <indy@pop-os.localdomain>
2023-05-19 16:54:51 -0400

    fixing

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -24,19 +24,15 @@ local freedesktop = require("freedesktop")
 require("awful.hotkeys_popup.keys")
 
 -- Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
     naughty.notify({ preset = naughty.config.presets.critical,
                      title = "Oops, there were errors during startup!",
                      text = awesome.startup_errors })
 end
 
--- Handle runtime errors after startup
 do
     local in_error = false
     awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
         if in_error then return end
         in_error = true
 
