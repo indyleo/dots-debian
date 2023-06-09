d13c09cf90aeca41636509e6b552188569e66250
Indy <indyprotonmail@proton.me>
2023-06-09 19:01:26 -0400

    fixes

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -1,17 +1,22 @@
 #!/bin/env bash
 
+# Fix Mouse curoor on startup 
+xsetroot -cursor_name left_ptr
+
+# Set Keyboard Layout
+setxkbmap -layout us
+
 # Killing Of Programs
-killall -q solaar &
-killall -q sxhkd &
-killall -q greenclip &
+killall -q solaar
+killall -q sxhkd
+killall -q greenclip
 
 # Starting Programs
-picom &
-sxhkd &
+picom --config "$HOME/.config/awesome/picom.conf" &
+sxhkd -c "$HOME/.config/awesome/sxhkdrc" &
 lxpolkit &
-sleep 1 && greenclip daemon &
+greenclip daemon &
 solaar -w hide &
 
 # Wallpaper
-# find ~/Pictures/Wallpapers/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
 xwallpaper --stretch ~/Pictures/Wallpapers/3xsraffkwi1a1.png &
