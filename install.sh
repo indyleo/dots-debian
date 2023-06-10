a0d997e1e637848ab25eadc2e8a43ea73805ec65
Indy <100316787+indyleo@users.noreply.github.com>
2023-06-10 14:41:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -128,7 +128,6 @@ nala install libreoffice zathura -y
 echo "##########################"
 echo "## Installing A Browser ##"
 echo "##########################"
-nala purge firefox -y
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
 nala update
