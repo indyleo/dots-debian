4674304bb2f0fd3710bc3d33de3249a830cd6bc5
Indy <Indy@debian.com>
2023-06-27 13:49:36 -0400

    windows spawn as slaves now

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -406,6 +406,7 @@ awful.rules.rules = {
 
 -- Signals
 client.connect_signal("manage", function (c)
+  if not awesome.startup then awful.client.setslave(c) end
     if awesome.startup
       and not c.size_hints.user_position
       and not c.size_hints.program_position then
