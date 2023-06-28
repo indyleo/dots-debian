00acc0e66c0a5ba1c591d1de7928aca16bad6a5e
Indy <Indy@debian.com>
2023-06-27 20:12:38 -0400

    changing up alot

--- install.sh
+++ install.sh
@@ -26,11 +26,9 @@ apt install nala -y
 echo "#######################"
 echo "## CLI Nice To Haves ##"
 echo "#######################"
-nala install direnv yad fzf locate gh tree build-essential git cmake \
-  make libhidapi-dev gpg openssl tldr trash-cli g++ gcc wget curl \
-  python3 unzip tar python3-setuptools autojump luarocks ranger \
-  shellcheck python3-venv meson lsd stow apt-transport-https qalc cmdtest \
-  qalc libtool libtool-bin ninja-build autoconf automake python3-pli bat -y
+nala install direnv yad fzf locate gh tree build-essential git cmake make libhidapi-dev gpg openssl tldr trash-cli g++ gcc wget curl python3 unzip tar \
+  python3-setuptools autojump luarocks ranger shellcheck python3-venv meson lsd stow apt-transport-https qalc cmdtest qalc libtool libtool-bin ninja-build \
+  autoconf automake python3-pil bat -y
 
 echo "##################"
 echo "## Wifi Manager ##"
@@ -80,24 +78,20 @@ nala install openjdk-11-jdk openjdk-11-jre openjdk-17-jdk openjdk-17-jre openjdk
 echo "#############################"
 echo "## Installing GUI Programs ##"
 echo "#############################"
-nala install polybar galculator kitty bleachbit timeshift \
-  transmission-gtk dconf-editor solaar virt-manager xinit sxhkd \
-  arandr xterm xclip rofi-dev xbacklight xsel xdotool screenkey \
-  gedit gedit-plugins xorg -y
+nala install polybar galculator kitty bleachbit timeshift transmission-gtk dconf-editor solaar virt-manager xinit sxhkd arandr xterm xclip rofi-dev xbacklight \
+  xsel xdotool screenkey gedit gedit-plugins xorg -y
 
 echo "##############################"
 echo "## Installing VIA (Keycron) ##"
 echo "##############################"
 wget -q https://github.com/the-via/releases/releases/download/v3.0.0/via-3.0.0-linux.deb
 nala install ./via-3.0.0-linux.deb -y
-rm ./via-3.0.0-linux.deb
+rm -v ./via-3.0.0-linux.deb
 
 echo "############################"
 echo "## Installing Media Stuff ##"
 echo "############################"
-nala install rhythmbox rhythmbox-plugins rhythmbox-data \
-  playerctl pamixer pavucontrol yt-dlp mpv peek obs-studio kdenlive \
-  audacity gimp vlc sxiv pulsemixer -y
+nala install rhythmbox rhythmbox-plugins rhythmbox-data playerctl pamixer pavucontrol yt-dlp mpv peek obs-studio kdenlive audacity gimp vlc sxiv pulsemixer -y
 
 echo "############################"
 echo "## Installing Theme Stuff ##"
@@ -107,7 +101,7 @@ nala install qt5ct qt5-style-plugins lxappearance -y
 echo "###########################"
 echo "## Installing File Stuff ##"
 echo "###########################"
-nala install pcmanfm file-roller -y
+nala install thunar thunar-archive-plugin thunar-media-tags-plugin thunar-font-manager gvfs file-roller -y
 
 echo "##################################"
 echo "## Installing Things For Any WM ##"
@@ -117,10 +111,9 @@ nala install xwallpaper flameshot lxpolkit rofi i3lock-fancy -y
 echo "#################################"
 echo "## Installing Things For Picom ##"
 echo "#################################"
-nala install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev \
-  libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev \
-  libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev \
-  libev-dev libx11-xcb-dev -y 
+nala install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev \
+  libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev \
+  libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev -y 
 
 echo "##########################"
 echo "## Installing AwesomeWN ##"
@@ -136,15 +129,15 @@ echo "##########################"
 echo "## Installing A Browser ##"
 echo "##########################"
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" \
+  | tee /etc/apt/sources.list.d/brave-browser-release.list
 nala update
 nala install brave-browser -y
 
 echo "#####################"
 echo "## Installing SDDM ##"
 echo "#####################"
-nala install sddm libqt5svg5 qml-module-qtquick-controls \
-  qml-module-qtquick-controls2 -y
+nala install sddm libqt5svg5 qml-module-qtquick-controls qml-module-qtquick-controls2 -y
 tar -xzvf sugar-candy.tar.gz -C /usr/share/sddm/themes
 mv sddm.conf /etc/sddm.conf
 systemctl enable sddm
