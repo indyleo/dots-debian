266cf84a509e3bbd533007cffa0d64b91c7e71c7
Indy <100316787+indyleo@users.noreply.github.com>
2023-07-16 20:37:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -118,7 +118,7 @@ echo "## Installing Things For Picom ##"
 echo "#################################"
 nala install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev \
   libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev \
-  libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev -y 
+  libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libx11-xcb1 libxcb-util-dev libxcb-util0-dev libxcb-util1 -y 
 
 echo "##########################"
 echo "## Installing AwesomeWN ##"
