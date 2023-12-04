07b913b2a13813afa6d5bf02ff7c4fdb59b2140e
Indy <Indy@debian.org>
2023-12-04 15:47:32 -0500

    muti monitor (will hopfuly fix poly bar soon)

--- polybar/polybar-startup-scripts/awesome-polybar.sh
+++ polybar/polybar-startup-scripts/awesome-polybar.sh
@@ -4,8 +4,8 @@
 killall -q polybar
 
 # Launch bars
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
 polybar awesome1 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar awesome2 2>&1 | tee -a /tmp/polybar2.log & disown
-polybar awesome3 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar awesome3 2>&1 | tee -a /tmp/polybar3.log & disown
 echo "Bars launched..."
