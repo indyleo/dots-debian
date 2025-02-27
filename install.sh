0fc740cfde3b75f041b50f2dd74aaccccb71f56f
Indy <Indy@LFS.local>
2025-02-27 16:32:38 -0500

    change neofetch to fastfetch, and change brave to floorp

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ nala install xdg-user-dirs xdg-user-dirs-gtk -y
 echo "###############"
 echo "## Fun Stuff ##"
 echo "###############"
-nala install neofetch cowsay cmatrix tty-clock lolcat -y
+nala install fastfetch cowsay cmatrix tty-clock lolcat -y
 
 echo "###############"
 echo "## USB Utils ##"
@@ -88,17 +88,6 @@ echo "## Installing GUI Programs ##"
 echo "#############################"
 nala install polybar galculator kitty bleachbit timeshift dconf-editor solaar piper virt-manager xinit sxhkd arandr xterm xclip rofi-dev xsel \
   xdotool xdo screenkey gedit gedit-plugins xorg xserver-xorg xserver-xephyr xautolock gparted -y
-
-echo "#########################"
-echo "## Installing Vscodium ##"
-echo "#########################"
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-    | gpg --dearmor \
-    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
-echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
-    | sudo tee /etc/apt/sources.list.d/vscodium.list
-nala update
-nala install codium -y
 
 echo "##############################"
 echo "## Installing VIA (Keycron) ##"
@@ -152,11 +141,10 @@ nala install libreoffice zathura -y
 echo "##########################"
 echo "## Installing A Browser ##"
 echo "##########################"
-curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" \
-  | tee /etc/apt/sources.list.d/brave-browser-release.list
+curl -fsSL https://ppa.floorp.app/KEY.gpg | gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
+curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.floorp.app/Floorp.list'
 nala update
-nala install brave-browser -y
+nala install floorp
 
 echo "#####################"
 echo "## Installing SDDM ##"
@@ -191,13 +179,6 @@ ufw default deny incoming
 ufw default allow outgoing
 ufw enable
 systemctl enable ufw
-
-echo "############"
-echo "## Pfetch ##"
-echo "############"
-wget https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch 
-chmod a+x pfetch
-mv -v pfetch /usr/bin/
 
 echo "#####################"
 echo "## Starship Prompt ##"
